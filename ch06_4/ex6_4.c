4923775dd163da8402a9c0acf783472143e17d53
jeseok2658 <andy-yu@naver.com>
2026-04-09 14:39:48 +0900

    Add files via upload

---
+++ ch06_4/ex6_4.c
@@ -0,0 +1,24 @@
+#include "deQue.h"
+#include <stdio.h>
+
+int main(void)
+{
+	DQueType* DQ1 = createDQue();
+	element data;
+	printf("\n ***** 데크 연산 ***** \n");
+	printf("\n front 삽입 A>> ");  insertFront(DQ1, 'A');  printDQ(DQ1);
+	printf("\n front 삽입 B>> ");  insertFront(DQ1, 'B');  printDQ(DQ1);
+	printf("\n rear  삽입 C>> ");  insertRear(DQ1, 'C');  printDQ(DQ1);
+	printf("\n front 삽입  >> ");  data = deleteFront(DQ1);    printDQ(DQ1);
+	printf("\t데이터 삭제: %c", data);
+	printf("\n rear  삭제  >> ");  data = deleteRear(DQ1);     printDQ(DQ1);
+	printf("\t\t데이터 삭제: %c", data);
+	printf("\n rear  삽입 D>> ");  insertRear(DQ1, 'D');  printDQ(DQ1);
+	printf("\n front 삽입 E>> ");  insertFront(DQ1, 'E');  printDQ(DQ1);
+	printf("\n front 삽입 F>> ");  insertFront(DQ1, 'F');  printDQ(DQ1);
+
+	data = peekFront(DQ1);  printf("\n peek Front item : %c \n", data);
+	data = peekRear(DQ1);  printf(" peek Rear item : %c \n", data);
+
+	getchar();  return 0;
+}
